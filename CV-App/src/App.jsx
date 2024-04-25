cfa51a28eb21c95280a235a638db7d28cb84e412
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-04-25 22:46:52 +0530

    Create about component

--- CV-App/src/App.jsx
+++ CV-App/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import About from './components/About'
 
 function App() {
 
-
   return (
     <>
-      <h1>Lets goo !</h1>
+      <About/>
     </>
   )
 }
---
+++ CV-App/src/components/About.jsx
@@ -0,0 +1,17 @@
+import "../styles/about.css"
+import React from 'react'
+
+function About() {
+  return (
+    <div className="container">
+        <h2 className="name">Abu Sadat Ansari</h2>
+        <div className="info">
+            <p className="email">itssadatansari@gmail.com</p>
+            <p className="number">+91 7477591090</p>
+            <p className="location">Kolkata</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
